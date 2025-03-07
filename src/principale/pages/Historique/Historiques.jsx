2857b9c12f3dfe82601f23932cc2d0fbba8774fc
Armel <armelbedjra12@gmail.com>
2025-03-07 18:48:44 +0000

    Description

--- src/principale/pages/Historique/Historiques.jsx
+++ src/principale/pages/Historique/Historiques.jsx
@@ -33,10 +33,10 @@ const Historique = () => {
 
   return (
 
-      <div className="historique">
+    <div className="historique">
 
-        <h2>Historique des Transactions</h2>
-
+      <h2>Historique des Transactions</h2>
+      <div className="label">
         <label htmlFor="periode">Filtrer par période :</label>
         <select id="periode" value={periode} onChange={(e) => setPeriode(e.target.value)}>
           <option value="">Toutes</option>
@@ -45,61 +45,63 @@ const Historique = () => {
           <option value="annee">Annuel</option>
         </select>
 
-        {loading ? (
-          <p>Chargement des transactions...</p>
-        ) : (
-          <table>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Date</th>
-                <th>Montant (FCFA)</th>
-                <th>Taux</th>
-                <th>Montant UDST </th>
-                <th>Fournisseurs</th>
-                <th>Bénéficiaires</th>
-                <th>Bénéfice Bénéficiaire</th>
-                <th>Bénéfice Total </th>
-              </tr>
-            </thead>
-            <tbody>
-              {transactions.length > 0 ? (
-                transactions.map((t, index) => (
-                  <tr key={index}>
-                    <td>{t.transaction_id}</td>
-                    <td>{new Date(t.date_transaction).toLocaleDateString()}</td>
-                    <td>{t.montant_FCFA} FCFA</td>
-                    <td>{t.taux_convenu}</td>
-                    <td>{t.montant_USDT}</td>
-                    <td>
-                      {t.benefices_fournisseurs.length > 0
-                        ? t.benefices_fournisseurs.map((f) => f.fournisseur).join(", ")
-                        : "Aucun"}
-                    </td>
-                    <td>
-                      {t.repartition_beneficiaires.length > 0
-                        ? t.repartition_beneficiaires.map((b) => b.beneficiaire).join(", ")
-                        : "Aucun"}
-                    </td>
-                    <td>
-                      {t.repartition_beneficiaires.length > 0
-                        ? t.repartition_beneficiaires.map((b) => b.benefice_FCFA).join(", ")
-                        : "0"} FCFA
-                    </td>
-                    <td>{t.resume_global.benefice_total_fournisseurs} FCFA</td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan="8" style={{ textAlign: "center" }}>
-                    Aucune transaction trouvée
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        )}
       </div>
+
+      {loading ? (
+        <p>Chargement des transactions...</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Date</th>
+              <th>Montant (FCFA)</th>
+              <th>Taux</th>
+              <th>Montant UDST </th>
+              <th>Fournisseurs</th>
+              <th>Bénéficiaires</th>
+              <th>Bénéfice Bénéficiaire</th>
+              <th>Bénéfice Total </th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.length > 0 ? (
+              transactions.map((t, index) => (
+                <tr key={index}>
+                  <td>{t.transaction_id}</td>
+                  <td>{new Date(t.date_transaction).toLocaleDateString()}</td>
+                  <td>{t.montant_FCFA} FCFA</td>
+                  <td>{t.taux_convenu}</td>
+                  <td>{t.montant_USDT}</td>
+                  <td>
+                    {t.benefices_fournisseurs.length > 0
+                      ? t.benefices_fournisseurs.map((f) => f.fournisseur).join(", ")
+                      : "Aucun"}
+                  </td>
+                  <td>
+                    {t.repartition_beneficiaires.length > 0
+                      ? t.repartition_beneficiaires.map((b) => b.beneficiaire).join(", ")
+                      : "Aucun"}
+                  </td>
+                  <td>
+                    {t.repartition_beneficiaires.length > 0
+                      ? t.repartition_beneficiaires.map((b) => b.benefice_FCFA).join(", ")
+                      : "0"} FCFA
+                  </td>
+                  <td>{t.resume_global.benefice_total_fournisseurs} FCFA</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="8" style={{ textAlign: "center" }}>
+                  Aucune transaction trouvée
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
